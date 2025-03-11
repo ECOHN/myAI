dc1376f24b7511bf9d48a3621de051d567f99a65
ECOHN <112498798+ECOHN@users.noreply.github.com>
2025-03-11 17:28:13 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Ethan`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `InvestiBot`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Professional`;
 export const AI_ROLE: string = `Stock Advisor`;
